5643a2b64bc649ad12950b0126886c833485345b
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-03-20 09:28:52 -0700

    Fix spacing

--- index.js
+++ index.js
@@ -213,14 +213,14 @@ function getByteOffset(pos, chromInfo) {
 // Random number generator with seed
 // https://stackoverflow.com/a/424445
 function RNG(seed) {
-  // LCG using GCC's constants
-  this.m = 0x80000000; // 2**31;
-  this.a = 1103515245;
-  this.c = 12345;
+	// LCG using GCC's constants
+	this.m = 0x80000000; // 2**31;
+	this.a = 1103515245;
+	this.c = 12345;
 
-  this.state = seed ? seed : Math.floor(Math.random() * (this.m - 1));
+	this.state = seed ? seed : Math.floor(Math.random() * (this.m - 1));
 }
 RNG.prototype.nextInt = function() {
-  this.state = (this.a * this.state + this.c) % this.m;
-  return this.state;
+	this.state = (this.a * this.state + this.c) % this.m;
+	return this.state;
 }
